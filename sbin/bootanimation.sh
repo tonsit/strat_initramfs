c69bda408fffb45bef46b09efab0641544ce2ec1
tonsit <tonsit@gmail.com>
2012-02-11 00:36:35 -0500

    Update to boot animation handling:  checks for and renames bootanimation.zip to sanim.zip

--- sbin/bootanimation.sh
+++ sbin/bootanimation.sh
@@ -1,6 +1,10 @@
 #!/sbin/sh
-#Checks for sanim.zip and runs the binary accordingly
+#Boot animation handler.
+#Catch bootanimation.zip and rename to sanim.zip
+if [ -f /system/media/bootanimation.zip]; then
+	mv /system/media/bootanimation.zip /system/media/sanim.zip
 
+#if sanim.zip exists, launch android bootanimation binary, otherwise launch default samsung animation binary
 if [ -f /system/media/sanim.zip ]; then
 	/system/bin/bootanimation
 else
